48da863441ac539c49899201210e752048bdee85
Tarik Wesley <tarik.costa@b2wdigital.com>
2022-07-20 19:55:16 -0300

    gerando figurinhas

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Map;
-
 public class Main {
   public static void main(String[] args) throws Exception {
     // fazer uma conexão HTTP e buscar os top 250 filmes
-    String url = "https://imdb-api.com/en/API/Top250Movies/k_g12ih037";
+    String url = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
     URI endereco = URI.create(url);
     var client = HttpClient.newBuilder().build();
     var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -21,10 +22,16 @@ public class Main {
     System.out.println(listaDeFilmes.size());
 
     // exibir e manipular os dados
+    var geradora = new GeradorDeFigurinhas();
     for (Map<String, String> filme : listaDeFilmes) {
+      String urlImagem = filme.get("image");
+      String titulo = filme.get("title");
+      String nomeArquivo = titulo + ".png";
+
+      InputStream inputStream = new URL(urlImagem).openStream();
+      geradora.cria(inputStream, nomeArquivo);
+
       System.out.println(filme.get("title"));
-      System.out.println(filme.get("image"));
-      System.out.println(filme.get("imDbRating"));
       System.out.println();
     }
   }
